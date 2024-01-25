8ef2a47f810cdac830f048c4d6a867e2c2829d18
Douglas <dougsrodrigues@outlook.com>
2024-01-25 09:29:35 -0300

    feat: create sign in page

--- src/modules/authentication/pages/sign-in.tsx
+++ src/modules/authentication/pages/sign-in.tsx
@@ -1,3 +1,33 @@
+import './styles.scss';
+
+import { zodResolver } from '@hookform/resolvers/zod';
+import { useForm } from 'react-hook-form';
+
+import { useSignIn } from '../hooks/useSignIn';
+import { signInSchema } from '../utils/schemas';
+import { type SignInSchemaData } from '../utils/types';
+
 export function SignIn() {
-  return <h1>sing in</h1>;
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SignInSchemaData>({
+    resolver: zodResolver(signInSchema),
+  });
+
+  const { handleSignIn } = useSignIn();
+
+  return (
+    <div className="container">
+      <form className="content" onSubmit={handleSubmit(handleSignIn)}>
+        <input {...register('email')} type="text" placeholder="e-mail" />
+        <input {...register('password')} type="text" placeholder="password" />
+
+        <button disabled={isSubmitting} type="submit">
+          Login
+        </button>
+      </form>
+    </div>
+  );
 }
