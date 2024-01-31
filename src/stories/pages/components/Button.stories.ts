0ef460f464c93d15d7467b7d082a0e4a4221e617
Douglas <dougsrodrigues@outlook.com>
2024-01-31 18:14:38 -0300

    refactor: remove unused code

--- src/stories/pages/components/Button.stories.ts
+++ src/stories/pages/components/Button.stories.ts
@@ -3,7 +3,6 @@ import type { Meta, StoryObj } from '@storybook/react';
 import { Button } from '@/modules/app/components/button';
 import { BUTTON_VARIANTS } from '@/modules/app/utils/constants';
 
-// More on how to set up stories at: https://storybook.js.org/docs/writing-stories#default-export
 const meta = {
   title: 'Example/Button',
   component: Button,
@@ -16,7 +15,6 @@ export default meta;
 
 type Story = StoryObj<typeof meta>;
 
-// More on writing stories with args: https://storybook.js.org/docs/writing-stories/args
 export const Primary: Story = {};
 
 export const Secondary: Story = {
